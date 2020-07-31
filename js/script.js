cc928ca31895e2235597252620b6ee9ad94015b5
Kieffer Navarro <kieffer.navarro@gmail.com>
2020-07-31 16:14:12 +0800

    first commit final output

---
+++ js/script.js
@@ -0,0 +1,14 @@
+	
+
+	function openNav() {
+	  document.getElementById("sidebar-kippy").style.width = "40%";
+	}
+
+	function openNav2() {
+	  document.getElementById("sidebar-kippy").style.width = "100%";
+	}
+
+
+	function closeNav() {
+	  document.getElementById("sidebar-kippy").style.width = "0";
+	}
